617b03660639f2de94484112cbd5c40b0dc58e4d
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-04-13 22:42:49 -0300

    add proyecto y diagrama de Clase-Cuenta

---
+++ proyecto-sist-comp/LABORATORIO-1/lab1Cuenta/src/lab1cuenta/cuentaApp.java
@@ -0,0 +1,11 @@
+
+package lab1cuenta;
+
+
+public class cuentaApp {
+    
+    public static void main(String[] args) {
+        
+        Lab1Cuenta c1 = new Lab1Cuenta("Pepe", "78109", 7.0, 1000);
+    }
+}
